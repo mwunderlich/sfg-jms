233ef8a603096dfbb35bbe8b903cbd91a3c4bf03
Martin Wunderlich <martin.wunderlich@censhare.com>
2020-04-26 11:38:51 +0200

    Add JMS listener

---
+++ src/main/java/guru/springframework/sfgjms/listener/HelloWorldListener.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) by censhare AG
+ */
+package guru.springframework.sfgjms.listener;
+
+import guru.springframework.sfgjms.config.JmsConfig;
+import guru.springframework.sfgjms.model.HelloWorldMessage;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Message;
+
+/**
+ * JSM listener
+ *
+ * @author Martin Wunderlich
+ */
+@Component
+public class HelloWorldListener {
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers, Message msg) {
+
+        System.out.println("msg received");
+
+        System.out.println(helloWorldMessage);
+    }
+}
